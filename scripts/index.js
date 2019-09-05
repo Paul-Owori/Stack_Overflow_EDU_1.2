2349ca4e9c1cffce3b0491d1381c153d26cea373
Paul-Owori <prowori2@gmail.com>
2019-09-05 16:44:57 +0300

    Initialize JQuery

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+// Javascript master
+
+$(document).ready(() => {
+
+    // $("#serve").click(()=>{
+
+    //   window.open('spSide/sp-signUp.html', '_self')
+
+    // });
+
+    // $("#order").click(()=>{
+
+    //   window.open('userSide/user.html' , '_self')
+
+    // });
+    console.log("Document ready..")
+
+});
