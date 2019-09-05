4dc466619a96da3534ad231192b0b94682f011c3
Paul-Owori <prowori2@gmail.com>
2019-09-05 17:52:36 +0300

    Fixed index.js

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
+// const express = require('express');
+
+// const app = express();
+// app.use(express.static('public'));
+
+
+// const port = process.env.PORT || 3000;
+
+
+// //app.set('port', process.env.PORT || 3000);
+// //app.listen(3000)
+
+// // app.get('/home', (req, res) => {
+// //     res.sendFile(__dirname + 'index.html');
+// // })
+
+
+// // app.get("/*", (req, res) => {
+// //     res.sendFile(path.join(__dirname + "/client/public/index.html"));
+
+// //   });
+
+// app.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname + 'index.html'));
+// });
+
+// app.listen(port);
+
 const express = require('express');
-
 const app = express();
-app.use(express.static('public'));
+const path = require('path');
 
-
-const port = process.env.PORT || 3000;
-
-
-//app.set('port', process.env.PORT || 3000);
-//app.listen(3000)
-
-// app.get('/home', (req, res) => {
-//     res.sendFile(__dirname + 'index.html');
-// })
-
-
-// app.get("/*", (req, res) => {
-//     res.sendFile(path.join(__dirname + "/client/public/index.html"));
-
-//   });
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(port);
+app.listen(process.env.PORT || 4000, () => {
+    console.log('Your node js server is running');
+});
