b023020ff05d6b2e5c5f7c1840857c2a173b1163
Paul-Owori <prowori2@gmail.com>
2019-09-05 18:13:09 +0300

    Fixed links to public folder in index.html

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-// const express = require('express');
-
-// const app = express();
-// app.use(express.static('public'));
-
-
-// const port = process.env.PORT || 3000;
-
-
-// //app.set('port', process.env.PORT || 3000);
-// //app.listen(3000)
-
-// // app.get('/home', (req, res) => {
-// //     res.sendFile(__dirname + 'index.html');
-// // })
-
-
-// // app.get("/*", (req, res) => {
-// //     res.sendFile(path.join(__dirname + "/client/public/index.html"));
-
-// //   });
-
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname + 'index.html'));
-// });
-
-// app.listen(port);
-
 const express = require('express');
 const app = express();
 const path = require('path');
 
 app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
